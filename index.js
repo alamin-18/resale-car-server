86f01382884e5f0f54ab652bb8923951e813ac89
Md Al Amin <fpalamin6@gmail.com>
2022-11-28 18:24:21 +0600

    vercel add

--- index.js
+++ index.js
@@ -7,10 +7,6 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 
-
-//resaleproduct
-//p7S9NetalykRy1zy
-
 // middleware
 app.use(cors());
 app.use(express.json());
@@ -20,7 +16,7 @@ app.get('/', async (req, res) => {
 })
 
 
-const uri = "mongodb+srv://resaleproduct:p7S9NetalykRy1zy@cluster0.zokrihv.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.zokrihv.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run() {
